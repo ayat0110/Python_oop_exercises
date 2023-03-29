122dfcd61aec0fc9dba177401894510c241a19ec
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-03-29 21:17:12 +0300

    Create first_exercis.py

---
+++ classes/first_exercis.py
@@ -0,0 +1,9 @@
+class Item:
+    def calculate_total_price(self,x,y):
+        return x*y
+    #functions inside classes called methods
+item1=Item()
+item1.name="Phone"
+item1.price=100
+item1.quantity=5
+print(item1.calculate_total_price(item1.price,item1.quantity))
