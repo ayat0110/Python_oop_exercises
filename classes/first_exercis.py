f0e1e8bc717a5289d850500fde4540e0a66971ec
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-03-29 21:24:13 +0300

    Update first_exercis.py

--- classes/first_exercis.py
+++ classes/first_exercis.py
@@ -1,11 +1,10 @@
 class Item:
-    def __init__(self):
-       print("im created")
+    def __init__(self,name):
+       print(f"im created:{name}")
     def calculate_total_price(self,x,y):
         return x*y
     #functions inside classes called methods
-item1=Item()
-item1.name="Phone"
+item1=Item("phone")
 item1.price=100
 item1.quantity=5
 print(item1.calculate_total_price(item1.price,item1.quantity))
