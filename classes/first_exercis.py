ac4731dcd0522e09f073e0a4d8fd8bfc6fe7ca53
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-03-30 21:24:24 +0300

    Update first_exercis.py

--- classes/first_exercis.py
+++ classes/first_exercis.py
@@ -1,3 +1,4 @@
+import csv
 class Item:
     all = []
     pay_rate=0.8
@@ -15,6 +16,15 @@ class Item:
         self.price=self.price*self.pay_rate
         #the pay_rate belongs to the item class itself you can either access
         # it from the class level or the instance level
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv','r') as f:
+            reader= csv.DictReader(f)
+    #to make this a class method we need to use @classmethod before the
+    #method itself and this method will take the class itself instead of the instance
+    #we created this method as a class method instead of an instance method because we have the data 
+    #the csv file so we dont have instanes here so we need to created as a class method
+        
     def calculate_total_price(self):
         return self.price*self.quantity
     def __repr__(self):
