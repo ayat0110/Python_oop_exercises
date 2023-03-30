76747244f767c2b65cba19a4cb33e36685abf537
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-03-30 22:25:50 +0300

    Update inhertance.py

--- inhertance/inhertance.py
+++ inhertance/inhertance.py
@@ -1,52 +1,70 @@
 import csv
 
+
 class Item:
+    pay_rate = 0.8 # The pay rate after 20% discount
     all = []
-    pay_rate=0.8
-    def __init__(self,name:str ,price: float,quantity=0):
-       assert quantity>=0, f"quantity {quantity} is not greater than or equal to zero"
-       assert price>=0, f"price {price} is not greater than or equal t zero"
-       self.name=name
-       self.price=price
-       self.quantity=quantity
-       Item.all.append(self)
-       #for each instance is going to be created  is going to be filled
-       # with the instances created below
-       print(f"im created:{name}")
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
     def apply_discount(self):
-        self.price=self.price*self.pay_rate
-        #the pay_rate belongs to the item class itself you can either access
-        # it from the class level or the instance level
+        self.price = self.price * self.pay_rate
+
     @classmethod
     def instantiate_from_csv(cls):
-        with open('items.csv','r') as f:
-            reader= csv.DictReader(f)
-            items=list(reader)
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
         for item in items:
             Item(
                 name=item.get('name'),
                 price=float(item.get('price')),
-                quantity=int(item.get(quantity))
+                quantity=int(item.get('quantity')),
             )
-    #in static methods the method does not we never
-    # send the object as the first argument
-    @staticmethod    
+
+    @staticmethod
     def is_integer(num):
-        if isinstance(num,float):
+        # We will count out the floats that are point zero
+        # For i.e: 5.0, 10.0
+        if isinstance(num, float):
+            # Count out the floats that are point zero
             return num.is_integer()
-        elif isinstance(num,int):
+        elif isinstance(num, int):
             return True
         else:
             return False
-    #to make this a class method we need to use @classmethod before the
-    #method itself and this method will take the class itself instead of the instance
-    #we created this method as a class method instead of an instance method because we have the data 
-    #the csv file so we dont have instances here so we need to created as a class method
-        
-    def calculate_total_price(self):
-        return self.price*self.quantity
+
     def __repr__(self):
-        return f"Item('{self.name}',{self.price},{self.quantity})"
-    #functions inside classes called methods
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
 
 class Phone(Item):
+    def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+        # Call to super function to have access to all attributes / methods
+        super().__init__(
+            name, price, quantity
+        )
+
+        # Run validations to the received arguments
+        assert broken_phones >= 0, f"Broken Phones {broken_phones} is not greater or equal to zero!"
+
+        # Assign to self object
+        self.broken_phones = broken_phones
+
+phone1 = Phone("jscPhonev10", 500, 5, 1)
+
+print(Item.all)
