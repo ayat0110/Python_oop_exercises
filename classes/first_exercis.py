d70492531146ce21bf141833d888f884704578f8
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-03-30 20:59:57 +0300

    Update first_exercis.py

--- classes/first_exercis.py
+++ classes/first_exercis.py
@@ -6,6 +6,9 @@ class Item:
        self.name=name
        self.price=price
        self.quantity=quantity
+       Item.all.append(self)
+       #for each instance is going to be created  is going to be filled
+       # with the instances created below
        print(f"im created:{name}")
     def apply_discount(self):
         self.price=self.price*self.pay_rate
@@ -14,9 +17,12 @@ class Item:
     def calculate_total_price(self):
         return self.price*self.quantity
     #functions inside classes called methods
-item1=Item("phone",5,9)
-print(item1.calculate_total_price())
-item2=Item("laptop",1000,3)
-item2.pay_rate=0.7
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)  
 #we can do this because at first it will search at the instance level and because the instance dosnt have this value 
 # it will assgin the value we have right now
+for instance in Item.all:
+    print(instance.name)
